57e9b3bff12d3565249624803d05be5c459c8dab
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-05-23 22:42:21 +0545

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,100 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Hare Krishna"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.center,
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey[400],
-                blurRadius: 4,
-                offset: Offset(2.0,5.0)
-              )
-            ],
-          color: Colors.teal,
-          gradient: LinearGradient(colors: [Colors.yellow, Colors.blue], begin: Alignment(1,1), end: Alignment(-1,-1))
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+                alignment: Alignment.center,
+                  child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [Colors.white, Colors.lightBlue], begin: Alignment(-1,-1), end: Alignment(1,1))
+                ),
+                // width: MediaQuery.of(context).size.width,
+                // height: MediaQuery.of(context).size.height,
+                width: 200,
+                height: 400,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children:[
+                    Container(
+                  padding: const EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[400],
+                        blurRadius: 4,
+                        offset: Offset(2.0,5.0)
+                      )
+                    ],
+                  color: Colors.teal,
+                  gradient: LinearGradient(colors: [Colors.yellow, Colors.blue], begin: Alignment(1,1), end: Alignment(-1,-1))
+                  ),
+                  child: Text("Hare Krishna Hare Rama", style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  )),
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[400],
+                        blurRadius: 4,
+                        offset: Offset(2.0,5.0)
+                      )
+                    ],
+                  color: Colors.teal,
+                  gradient: LinearGradient(colors: [Colors.white, Colors.red], begin: Alignment(1,1), end: Alignment(-1,-1))
+                  ),
+                  child: Text("Hare Rama Hare Krishna", style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  )),
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[400],
+                        blurRadius: 4,
+                        offset: Offset(2.0,5.0)
+                      )
+                    ],
+                  color: Colors.teal,
+                  gradient: LinearGradient(colors: [Colors.yellow, Colors.green], begin: Alignment(1,1), end: Alignment(-1,-1))
+                  ),
+                  child: Text("Hare Krishna Hare Rama", style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  )),
+              ),
+              
+                  ]
+              ),
+                ),
           ),
-          child: Text("Hare Krishna Hare Rama", style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          )),
         ),
-      )
+      ),
     );
   }
 }
